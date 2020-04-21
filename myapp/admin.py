88f40ffba3bf5e4a469723923b0593ae450c29c3
Anurag Mandal <anurag_mandal@ymail.com>
2020-04-22 00:41:55 +0530

    Add models and minor changes

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User
+from .models import ManualInputDB
 # Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(ManualInputDB)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+
+
+class ManualInputDB(models.Model):
+    description = models.CharField(max_length=50)
+    category = models.CharField(max_length=20)
+    cost = models.AutoField
+    doft = models.DateField()
+
