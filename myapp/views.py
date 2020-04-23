25fbe127eb430c8628c9857cc35a9904ad7b4b7f
Anurag Mandal <anurag_mandal@ymail.com>
2020-04-24 03:20:17 +0530

    Add Graphs and Calculate total of Categories

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
+
 from myapp.models import Transaction
 from mysite.predict import predict
 
@@ -21,12 +22,112 @@ def register(request):
 
 
 def dashboard(request):
+    transactions_automobile = Transaction.objects.filter(user_id=request.user.id, category="Automobile")
+    total_automobile = 0
+    for transaction in transactions_automobile:
+        total_automobile = total_automobile + transaction.cost
+
+    transactions_bank = Transaction.objects.filter(user_id=request.user.id, category="Bank Transfer")
+    total_bank = 0
+    for transaction in transactions_bank:
+        total_bank = total_bank + transaction.cost
+
+    transactions_cash = Transaction.objects.filter(user_id=request.user.id, category="Cash Withdrawal")
+    total_cash = 0
+    for transaction in transactions_cash:
+        total_cash = total_cash + transaction.cost
+
+    transactions_education = Transaction.objects.filter(user_id=request.user.id, category="Education")
+    total_education = 0
+    for transaction in transactions_education:
+        total_education = total_education + transaction.cost
+
+    transactions_entertainment = Transaction.objects.filter(user_id=request.user.id, category="Entertainment")
+    total_entertainment = 0
+    for transaction in transactions_entertainment:
+        total_entertainment = total_entertainment + transaction.cost
+
+    transactions_fine = Transaction.objects.filter(user_id=request.user.id, category="Fine")
+    total_fine = 0
+    for transaction in transactions_fine:
+        total_fine = total_fine + transaction.cost
+
+    transactions_food = Transaction.objects.filter(user_id=request.user.id, category="Food")
+    total_food = 0
+    for transaction in transactions_food:
+        total_food = total_food + transaction.cost
+
+    transactions_health = Transaction.objects.filter(user_id=request.user.id, category="Health Care")
+    total_health = 0
+    for transaction in transactions_health:
+        total_health = total_health + transaction.cost
+#TODO - EXCHANGE IGNORE WITH OTHER
+    transactions_ignore = Transaction.objects.filter(user_id=request.user.id, category="Ignore")
+    total_ignore = 0
+    for transaction in transactions_ignore:
+        total_ignore = total_ignore + transaction.cost
+
+    transactions_paytm = Transaction.objects.filter(user_id=request.user.id, category="Paytm")
+    total_paytm = 0
+    for transaction in transactions_paytm:
+        total_paytm = total_paytm + transaction.cost
+
+    transactions_shopping = Transaction.objects.filter(user_id=request.user.id, category="Shopping")
+    total_shopping = 0
+    for transaction in transactions_shopping:
+        total_shopping = total_shopping + transaction.cost
+
+    transactions_travel = Transaction.objects.filter(user_id=request.user.id, category="Travel")
+    total_travel = 0
+    for transaction in transactions_travel:
+        total_travel = total_travel + transaction.cost
+
+    transactions_upi = Transaction.objects.filter(user_id=request.user.id, category="UPI")
+    total_upi = 0
+    for transaction in transactions_upi:
+        total_upi = total_upi + transaction.cost
+
+    transactions_recharge = Transaction.objects.filter(user_id=request.user.id, category="Recharge")
+    total_recharge = 0
+    for transaction in transactions_recharge:
+        total_recharge = total_recharge + transaction.cost
+
     transactions = Transaction.objects.filter(user_id=request.user.id)
-    final = 0
+    total = 0
     for transaction in transactions:
-        final = final + transaction.cost
-    print(final)
-    return render(request, 'dashboard.html', {'transactions': transactions, 'final': final})
+        total = total + transaction.cost
+
+    total_value = [total_automobile, total_bank, total_cash, total_education,
+                   total_entertainment, total_fine, total_food, total_health,
+                   total_ignore, total_paytm, total_shopping, total_travel,
+                   total_upi, total_recharge]
+
+    categories = ['Automobile', 'Bank Transfer', 'Cash Withdrawal', 'Education',
+                  'Entertainment', 'Fine', 'Food', 'Health Care',
+                   'Ignore', 'PayTM', 'Shopping', 'Travel',
+                   'UPI', 'Recharge']
+
+    return render(request, 'dashboard.html', {
+        'total_automobile': total_automobile,
+        'total_bank' : total_bank,
+        'total_cash': total_cash,
+        'total_education': total_education,
+        'total_entertainment': total_entertainment,
+        'total_fine': total_fine,
+        'total_food': total_food,
+        'total_health': total_health,
+        'total_ignore': total_ignore,
+        'total_paytm': total_paytm,
+        'total_shopping': total_shopping,
+        'total_travel': total_travel,
+        'total_upi': total_upi,
+        'total_recharge': total_recharge,
+        'total': total,
+        'total_value': total_value,
+        'transactions': transactions,
+        'categories': categories,
+
+    })
 
 
 def manual(request):
@@ -78,8 +179,9 @@ def handlePredict(request):
         transaction = request.POST['transaction']
     else:
         return HttpResponseNotFound('<h1>Error 404 - Page not found</h1>')
-    prediction = predict(transaction)
-    return HttpResponse(prediction)
+    prediction = predict(transaction)[0]
+    return render(request, 'dashboard.html', {'prediction': prediction})
+    #return HttpResponse(prediction)
 
 
 def manualAdd(request):
